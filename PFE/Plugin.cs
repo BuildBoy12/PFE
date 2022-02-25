fd23a7b87d6e7d55ab0c06f65e8ce3fdcdecef0b
Build <jwbradley2003@gmail.com>
2022-02-24 22:39:08 -0500

    Fix version

--- PFE/Plugin.cs
+++ PFE/Plugin.cs
@@ -27,9 +27,6 @@ namespace PFE
         public override string Prefix => "PFE";
 
         /// <inheritdoc />
-        public override Version Version { get; } = new Version(1, 0, 0);
-
-        /// <inheritdoc />
         public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
         /// <inheritdoc />
